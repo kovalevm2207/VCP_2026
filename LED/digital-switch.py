1b020972dbea6afee3ee1e37a68009c540aa9efd
kovalevm2207 <kovalevm220741@gmail.com>
2026-04-07 15:12:21 +0300

    скрипт на питоне: меняем состояние светодиода по нажатию кнопки

---
+++ LED/digital-switch.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time                # импортируем модули времени и работы с GPIO
+
+GPIO.setmode(GPIO.BCM)      # задаем режим обращения к портам
+led = 26                    # перменная с номером GPIO пина
+GPIO.setup(led, GPIO.OUT)   # настраиваем переменную как цифровой выход
+
+button = 13                 # номер GPIO пина кнопки
+GPIO.setup(button, GPIO.IN) # настраиваем переменную как цифровой вход
+
+state = 0
+
+while True:
+    if GPIO.input(button):
+        state = not state
+        GPIO.output(led, state)
+        time.sleep(0.5)
